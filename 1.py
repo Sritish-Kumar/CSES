d99d6a754c412b047a793279ff1212834ee549ab
sritish <sritishkumargouda@gmail.com>
2023-09-30 23:47:06 +0530

    1 done

--- 1.py
+++ 1.py
@@ -1,1 +1,17 @@
-print('hello')
+#Weird Algorithm
+
+n=int(input())
+print(n,end=' ')
+while n!=1:
+    
+    if n%2==0:
+        #print('even')
+        n=int(n/2)
+        
+    else:
+        #print('odd')
+        n=(n*3)+1
+    
+    print(n,end=' ')
+
+    
