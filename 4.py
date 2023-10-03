3568ca1e285d35a4151ea06da48744770a31d6e0
sritish <sritishkumargouda@gmail.com>
2023-10-03 19:56:24 +0530

    increasing array done

---
+++ 4.py
@@ -0,0 +1,23 @@
+# increasing array
+
+n=int(input())
+x=list(map(int,input().strip().split(' ')))  # [6,10,4,10,2,8,9,2,7,7]  
+
+c=0
+t=0
+max=0
+
+for i in range(len(x)):
+    if max<=x[i]:
+        c=x[i]
+        if c>max:
+            max=c
+        continue
+    elif max>x[i]:
+        #print('*',max-x[i])
+        t=t+(max-x[i])
+        #print(t)
+        c=x[i]
+        continue
+    
+print(t)
