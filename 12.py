148335728ed0ccd773c17d2d6fac9d19c2b0e5ce
sritish <sritishkumargouda@gmail.com>
2023-10-14 01:57:37 +0530

    Gray code ( under construction)

---
+++ 12.py
@@ -0,0 +1,17 @@
+# Graay code
+
+n=int(input())
+l=1
+i=0
+c=0                         # all the bin value under the reqired len
+while l<=n:
+    if len(bin(i)[2:])!=n:
+        print(i, (n-len(bin(i)[2:]))*'0'+bin(i)[2:])     
+    else:
+        print(i, bin(i)[2:])
+    i+=1
+    c+=1
+    if len(bin(i)[2:])>l:
+        l=len(bin(i)[2:])
+    
+#print(c)
