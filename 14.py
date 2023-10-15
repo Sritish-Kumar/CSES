f137a4deb1bf9f24c837b1c3ca65b870c32285ac
sritish <sritishkumargouda@gmail.com>
2023-10-16 00:29:58 +0530

    Two set ( Work in progress)

---
+++ 14.py
@@ -0,0 +1,18 @@
+# Sets
+
+num=[i for i in range(int(input()))]
+
+if sum(num)%2!=0:
+    # odd
+    print('Not possible')
+else:
+    # even
+
+    req=sum(num)//2
+    seta=[]
+
+    # Try adding elements to make it to the required number. 
+
+# not sure it will work there may be some even sum(num) which cant be divided
+
+# ................. ):
