7a48dbb58ffcc285ba595f8911970f776a668b75
sritish <sritishkumargouda@gmail.com>
2023-10-16 19:49:53 +0530

    Gray code done

--- 12.py
+++ 12.py
@@ -6,15 +6,26 @@
 n=int(input())
 l=1
 i=0
-c=0                         # all the bin value under the reqired len
+
+def gray(n):
+    x=''                    # converting bin to gray
+   
+    x+=n[0]
+    i=1
+    while i<len(n):
+        c=int(n[i]) ^ int(n[i-1])
+        x+=str(c)
+        i+=1
+    return x
+
+
 while l<=n:
-    if len(bin(i)[2:])!=n:
-        print(i, (n-len(bin(i)[2:]))*'0'+bin(i)[2:])     
+    if len(bin(i)[2:])!=n:   # making the required len bin
+        bn=(n-len(bin(i)[2:]))*'0'+bin(i)[2:]   
     else:
-        print(i, bin(i)[2:])
+        bn=bin(i)[2:]
+    gn=gray(bn)  # get gay
+    print(gn)
     i+=1
-    c+=1
-    if len(bin(i)[2:])>l:
+    if len(bin(i)[2:])>l:       # len updating
         l=len(bin(i)[2:])
-    
-#print(c)
