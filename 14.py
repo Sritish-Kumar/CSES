a341b9ad0c5a11e13e846747d43a101691f41e5e
sritish <sritishkumargouda@gmail.com>
2023-10-17 16:09:36 +0530

    Sets V1. ( still working )

--- 14.py
+++ 14.py
@@ -1,18 +1,41 @@
-# Sets
+# Sets ( working )
 
-num=[i for i in range(int(input()))]
+num=[i for i in range(1, int(input())+1)]
+num1=num
 
 if sum(num)%2!=0:
     # odd
-    print('Not possible')
+    print('NO')
 else:
     # even
 
     req=sum(num)//2
-    seta=[]
+    seta=[num.pop()]
 
-    # Try adding elements to make it to the required number. 
+    # Try adding elements to make it to the required number.
 
-# not sure it will work there may be some even sum(num) which cant be divided
+    for i in num:
+        if i+sum(seta) == req:
+            seta.append(i)
+            break
+        elif i+sum(seta) < req:
+            seta.append(i)
+            if req-sum(seta) in num and req-sum(seta)!=i:
+        
+                seta.append(req-sum(seta))
+                break
+            continue
 
-# ................. ):
+        elif i + sum(seta)> req:
+            continue
+
+    print('YES')
+
+    print(len(seta))
+    print(' '.join(list(map(str,seta))), sum(seta))
+    setb=set(num1)-set(seta)
+    print(len(setb))
+    print(' '.join(list(map(str, setb))), sum(setb))
+
+
+    
